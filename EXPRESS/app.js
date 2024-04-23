f1870b72d76289fb0cbccc95f312a11d3e8dc9e0
minsky922 <minsky922@gmail.com>
2024-04-23 23:54:30 +0900

    first

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,14 +1,52 @@
-var createError = require('http-errors');
+global.__base = __dirname + "/";
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 var logger = require('morgan');
+var cors = require("cors");
+
+var createError = require('http-errors');
+var cookieParser = require('cookie-parser');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+
+
+var server = require('https').createServer(app);
+
+var io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  },
+});
+
+
+
+server.listen(3001, function () {
+  console.log("Socket IO server listening on port 3001");
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('ping', msg => {
+    console.log(msg);
+    socket.emit('pong', {
+      msg: new Date().getTime()
+    })
+  })
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -23,12 +61,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
